349994b1fec2fcfcfe5102a1872b370eba366eee
viradeth xay-ananh <xayananh4@gmail.com>
2023-01-11 10:22:07 -0800

    add increasing font size
    Co-authored-by: Michael Sol <sol.mike.j@gmail.com>

--- js/shoutScript.js
+++ js/shoutScript.js
@@ -1,8 +1,11 @@
+let fontSize = 1;
 
 function getAndSetInputValues() {
+  let fontSizeStr = fontSize + "px";
+  fontSize += 1;
   let inputText = document.getElementById("input").value;
+  document.getElementById("test1").style.fontSize = fontSizeStr;
   document.querySelector("span.shout").innerText = inputText.toUpperCase();
-
 }
 
 function setFormSubmissionEventHandler() {
